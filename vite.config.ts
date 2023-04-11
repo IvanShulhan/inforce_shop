269736768353a7e6f27fce1ab1f41a8856a93aab
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2023-04-11 11:18:43 +0300

    add json server to project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 const cwd = process.cwd();
 const REPOSITORY_NAME = cwd.split('\\').pop();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: `/${REPOSITORY_NAME}/`
